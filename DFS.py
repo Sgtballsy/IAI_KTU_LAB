5790f6c4639c04bd6b29ac5c8df5674402eb8bb4
havisvh <havisvh35@gmail.com>
2024-11-03 21:10:44 +0530

    8puzzle heuristics and dfs added

---
+++ DFS.py
@@ -0,0 +1,28 @@
+print("pls enter the number of nodes: ")
+n = int(input())
+
+print("pls enter the start node: ")
+start = input()
+
+print("pls enter all the nodes: ")
+nodes = input().split()
+
+tree = {}
+for i in range(n):
+    print("pls enter the nodes connected to ", nodes[i], " : ")
+    tree[nodes[i]] = input().split()
+
+
+
+visited = []
+
+def dfs(node,tree,visited):
+    if node not in visited:
+        print(node,end=" ")
+        visited.append(node)
+        for neighbour in tree[node]:
+            dfs(neighbour,tree,visited)
+
+
+
+dfs(start,tree,visited)
